c6c8cb4599757929880fa8c5c72178b641e1a9df
Alexander Van Cappellen <alexvancap@gmail.com>
2019-10-17 23:14:16 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,8 @@
 def run_guessing_game
-  random_num = rand(6) + 1
+  random_num = (rand(6) + 1).to_s
   puts "Guess a number between 1 and 6."
   input = gets.chomp
+    if random_num == input
 
 
 end
